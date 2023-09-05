0aaae42301b47319fce0f004a34e99f44ef56e45
Jiayi Zeng <joyce.j.zeng@outlook.com>
2023-09-05 16:35:02 +0800

    Update QA System

--- pages/models.py
+++ pages/models.py
@@ -10,15 +10,34 @@ class Pages(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('body'),
-        InlinePanel('questions', label="Questions"),
+        InlinePanel('multipleChoice', label="MultipleChoice"),
+        InlinePanel('clozeQuestion', label="ClozeQuestion"),
     ]
 
-class Questions(Orderable):
-    page = ParentalKey(Pages, on_delete=models.CASCADE, related_name='questions')
+class MultipleChoice(Orderable):
+    page = ParentalKey(Pages, on_delete=models.CASCADE, related_name='multipleChoice')
     
     question = RichTextField(blank=True)
     answer = models.CharField(blank=True, max_length=250)
+    optionA = models.CharField(blank=True, max_length=250)
+    optionB = models.CharField(blank=True, max_length=250)
+    optionC = models.CharField(blank=True, max_length=250)
+    optionD = models.CharField(blank=True, max_length=250)
 
+    panels = [
+        FieldPanel('question'),
+        FieldPanel('answer'),
+        FieldPanel('optionA'),
+        FieldPanel('optionB'),
+        FieldPanel('optionC'),
+        FieldPanel('optionD'),
+    ]
+
+class ClozeQuestion(Orderable):
+    page = ParentalKey(Pages, on_delete=models.CASCADE, related_name='clozeQuestion')
+    
+    question = RichTextField(blank=True)
+    answer = models.CharField(blank=True, max_length=250)
     panels = [
         FieldPanel('question'),
         FieldPanel('answer'),
