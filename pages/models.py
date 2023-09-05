c34b507a571305cb8f2d55b0b0268d4409dfb9b2
Jiayi Zeng <joyce.j.zeng@outlook.com>
2023-09-05 15:04:18 +0800

    Change QA UI

--- pages/models.py
+++ pages/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
-from wagtail.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
-
+from wagtail.admin.panels import FieldPanel, InlinePanel
 
 class Pages(Page):
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body')
+        FieldPanel('body'),
+        InlinePanel('questions', label="Questions"),
+    ]
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(Pages, on_delete=models.CASCADE, related_name='questions')
+    
+    question = RichTextField(blank=True)
+    answer = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('question'),
+        FieldPanel('answer'),
     ]
