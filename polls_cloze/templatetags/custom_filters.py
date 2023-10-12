bf7e0c883f532df9e0ae750976cd873a5390cc0b
Jiayi Zeng <joyce.j.zeng@outlook.com>
2023-10-12 19:26:39 +0800

    Delete

--- polls_cloze/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='get_value_from_key')
-def get_value_from_key(dictionary, key):
-    return dictionary.get(str(key), 0)
--- question/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "question"
-
-urlpatterns = [path("", views.QuestionView.as_view(), name="question")]
