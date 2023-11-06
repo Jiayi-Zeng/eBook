c949e1b5116592c3d0f3ed2005043b71cf1aad2e
Jiayi Zeng <joyce.j.zeng@outlook.com>
2023-11-06 23:46:51 +0800

    Change Gitee into OSS

--- eBook/settings/base.py
+++ eBook/settings/base.py
@@ -165,7 +165,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "https://gitee.com/Jiayi-Zeng/picgo_markdown/raw/master/static/"
+STATIC_URL = "https://ebook2023.oss-cn-shanghai.aliyuncs.com/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
 
