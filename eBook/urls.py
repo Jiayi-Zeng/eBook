c4ab10ccb811d14bacb8ecbb03c56695a8b4d2da
fenggwsx <49786884+fenggwsx@users.noreply.github.com>
2023-12-11 14:34:45 +0800

    Update urls.py

--- eBook/urls.py
+++ eBook/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.auth.models import User
 
 from wagtail.admin import urls as wagtailadmin_urls
